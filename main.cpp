3cf316494112e8b8faa533f5c5e9d20e584a018e
ChuzaWick420 <zaiddadev@gmail.com>
2024-08-19 22:14:09 +0500

    Added: Sphere
    - Bug: img dimensions were switched in img header
    - Bug: formula for height based on aspect was wrong

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@
 #include "./headers/color3.hpp"
 #include "./headers/ray.hpp"
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+    if(hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
@@ -18,7 +30,10 @@ int main () {
     float aspect_ratio = 16.0/9.0;
 
     img_width = 400.0;
-    img_height = aspect_ratio * img_width;
+    img_height = img_width / aspect_ratio;
+
+    //height correction
+    img_height = (img_height < 1) ? 1 : img_height;
 
     //image creation
     std::ofstream file_ptr("image.ppm");
@@ -26,7 +41,7 @@ int main () {
     //image headers
     file_ptr << 
         "P3\n" << 
-        int(img_height) << " " << int(img_width) << "\n" <<
+        int(img_width) << " " << int(img_height) << "\n" <<
         255 << "\n";
 
     //camera stuff
