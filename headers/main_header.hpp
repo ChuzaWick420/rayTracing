f9963cbcbea8e861c8b210441e22dd36f008cf4f
ChuzaWick420 <zaiddadev@gmail.com>
2024-08-20 22:00:56 +0500

    Made a new header

---
+++ headers/main_header.hpp
@@ -0,0 +1,32 @@
+#ifndef MAIN_HEADER_HPP
+#define MAIN_HEADER_HPP
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+
+// C++ Std Usings
+
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility Functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common Headers
+
+#include "color3.hpp"
+#include "ray.hpp"
+#include "vec3.hpp"
+
+#endif
