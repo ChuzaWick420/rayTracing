1b35aa52dc83b573f53c214ffff55fb66bf55448
ChuzaWick420 <zaiddadev@gmail.com>
2024-08-20 21:59:34 +0500

    Added: Sphere
    Re-factored a lot of code and moved some things out into their own files

--- main.cpp
+++ main.cpp
@@ -1,33 +1,20 @@
-#include <iostream>
 #include <fstream>
 
-#include "./headers/vec3.hpp"
-#include "./headers/color3.hpp"
-#include "./headers/ray.hpp"
+#include "./headers/main_header.hpp"
+#include "./headers/hittable.hpp"
+#include "./headers/hittable_list.hpp"
+#include "./headers/sphere.hpp"
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
 
-    if (discriminant < 0)
-        return -1;
-    else
-        return ((-b - std::sqrt(discriminant)) / (2.0 * a));
-}
-
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.0, 0.0, 0.0);
 }
 
 int main () {
@@ -50,6 +37,12 @@ int main () {
         "P3\n" << 
         int(img_width) << " " << int(img_height) << "\n" <<
         255 << "\n";
+
+    // World
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     //camera stuff
     auto viewport_height = 2.0;
@@ -77,7 +70,7 @@ int main () {
             auto ray_direction = pixel_center - camera_center;
 
             ray r(camera_center, ray_direction);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(file_ptr, pixel_color);
         }
     }
