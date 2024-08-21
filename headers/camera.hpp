4606a16563d0a0df8ccb9e0bf615237ccfc56dbc
ChuzaWick420 <zaiddadev@gmail.com>
2024-08-22 03:26:35 +0500

    Added: Anti-Aliasing

--- headers/camera.hpp
+++ headers/camera.hpp
@@ -9,6 +9,7 @@ class camera {
 
         double aspect_ratio = 1.0;  //default aspect ratio is a square
         int img_width = 100;        //default image width is 100 pixels
+        int samples_per_pixel = 10; //count for random samples per pixel
 
         void render(const hittable& world) {
             initialize();
@@ -16,12 +17,14 @@ class camera {
             //writer loop
             for (int i = 0; i < img_height; i++) {
                 for (int j = 0; j < img_width; j++) {
-                    auto pixel_center = pixel00_loc + (j * pixel_delta_u) + (i * pixel_delta_v);
-                    auto ray_direction = pixel_center - center;
+                    color pixel_color(0, 0, 0);
 
-                    ray r(center, ray_direction);
-                    color pixel_color = ray_color(r, world);
-                    write_color(file_ptr, pixel_color);
+                    for (int sample = 0; sample < samples_per_pixel; sample++) {
+                        ray r = get_ray(j, i);
+                        pixel_color += ray_color(r, world);
+                    }
+
+                    write_color(file_ptr, pixel_color * pixel_samples_scale);
                 }
             }
         }
@@ -33,18 +36,21 @@ class camera {
 
     private:
 
-        std::ofstream file_ptr;                 //image to be rendered
+        std::ofstream file_ptr;                 // image to be rendered
         int           img_height;               // Rendered image height
         point3        center;                   // Camera center
         point3        pixel00_loc;              // Location of pixel 0, 0
         vec3          pixel_delta_u;            // Offset to pixel to the right
         vec3          pixel_delta_v;            // Offset to pixel below
+        double        pixel_samples_scale;      // Color scale factor for a sum of pixel samples
 
         void initialize() {
             file_ptr.open("image.ppm");
 
             img_height = int(img_width / aspect_ratio);
             img_height = (img_height < 1) ? 1 : img_height;
+
+            pixel_samples_scale = 1.0 / samples_per_pixel;
 
             //write image headers
             file_ptr << 
@@ -83,6 +89,27 @@ class camera {
             vec3 unit_direction = unit_vector(r.direction());
             auto a = 0.5*(unit_direction.y() + 1.0);
             return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+        }
+
+        ray get_ray(int u, int v) const {
+
+            // Construct a camera ray originating from the origin and directed at randomly sampled
+            // point around the pixel location i, j.
+
+            auto offset = sample_square();
+            auto pixel_sample = pixel00_loc
+                          + ((u + offset.x()) * pixel_delta_u)
+                          + ((v + offset.y()) * pixel_delta_v);
+
+            auto ray_origin = center;
+            auto ray_direction = pixel_sample - ray_origin;
+
+            return ray(ray_origin, ray_direction);
+        }
+
+        vec3 sample_square() const {
+            // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
+            return vec3(random_double() - 0.5, random_double() - 0.5, 0);
         }
 
 };
