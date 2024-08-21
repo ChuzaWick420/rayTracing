5409cecf2d03c3affa2b8eaa95bb49ef97b94db5
ChuzaWick420 <zaiddadev@gmail.com>
2024-08-21 22:00:34 +0500

    Fixed whitespace

--- headers/camera.hpp
+++ headers/camera.hpp
@@ -40,51 +40,50 @@ class camera {
         vec3          pixel_delta_u;            // Offset to pixel to the right
         vec3          pixel_delta_v;            // Offset to pixel below
 
-    void initialize() {
+        void initialize() {
+            file_ptr.open("image.ppm");
 
-        file_ptr.open("image.ppm");
+            img_height = int(img_width / aspect_ratio);
+            img_height = (img_height < 1) ? 1 : img_height;
 
-        img_height = int(img_width / aspect_ratio);
-        img_height = (img_height < 1) ? 1 : img_height;
+            //write image headers
+            file_ptr << 
+                "P3\n" << 
+                int(img_width) << " " << int(img_height) << "\n" <<
+                255 << "\n";
 
-        //write image headers
-        file_ptr << 
-            "P3\n" << 
-            int(img_width) << " " << int(img_height) << "\n" <<
-            255 << "\n";
+            center = point3(0, 0, 0);
 
-        center = point3(0, 0, 0);
+            // Determine viewport dimensions.
+            auto focal_length = 1.0;
+            auto viewport_height = 2.0;
+            auto viewport_width = viewport_height * (double(img_width)/img_height);
 
-        // Determine viewport dimensions.
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
-        auto viewport_width = viewport_height * (double(img_width)/img_height);
+            // Calculate the vectors across the horizontal and down the vertical viewport edges.
+            auto viewport_u = vec3(viewport_width, 0, 0);
+            auto viewport_v = vec3(0, -viewport_height, 0);
 
-        // Calculate the vectors across the horizontal and down the vertical viewport edges.
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+            // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+            pixel_delta_u = viewport_u / img_width;
+            pixel_delta_v = viewport_v / img_height;
 
-        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-        pixel_delta_u = viewport_u / img_width;
-        pixel_delta_v = viewport_v / img_height;
-
-        // Calculate the location of the upper left pixel.
-        auto viewport_upper_left =
-            center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-            pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-    }
-
-    color ray_color(const ray& r, const hittable& world) const {
-        hit_record rec;
-
-        if (world.hit(r, interval(0, infinity), rec)) {
-            return 0.5 * (rec.normal + color(1,1,1));
+            // Calculate the location of the upper left pixel.
+            auto viewport_upper_left =
+                center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+                pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
         }
 
-        vec3 unit_direction = unit_vector(r.direction());
-        auto a = 0.5*(unit_direction.y() + 1.0);
-        return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
-    }
+        color ray_color(const ray& r, const hittable& world) const {
+            hit_record rec;
+
+            if (world.hit(r, interval(0, infinity), rec)) {
+                return 0.5 * (rec.normal + color(1,1,1));
+            }
+
+            vec3 unit_direction = unit_vector(r.direction());
+            auto a = 0.5*(unit_direction.y() + 1.0);
+            return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+        }
 
 };
 
