699bd3ac602b1ef789095fccef1eab835b0ab059
ChuzaWick420 <zaiddadev@gmail.com>
2024-08-24 16:14:49 +0500

    Added: VS Code default color scheme to code block

--- docs/generator.js
+++ docs/generator.js
@@ -1,7 +1,14 @@
 async function generate() {
-	let data = await fetch("target.md");
-	console.log(data);
-	document.getElementById('content').innerHTML = marked.parse();
+	//get the markdown file
+	let md_file = await fetch("target.md");
+
+	//convert data into string
+	let data = await md_file.text();
+
+	//convert markdown string into html
+	document.getElementById('content').innerHTML = marked.parse(data);
+
+	//syntax highlight code blocks
 	hljs.highlightAll();
 }
 
--- docs/target.md
+++ docs/target.md
@@ -1,9 +1,22 @@
 # Hello World
 
+The following `code block` shows syntax highlighting
 ```cpp
 #include <iostream>
+
+int global = 69;
+
 int main () {
+
+    float hi = 69.40;
+
+    if (hi < 10)
+        printf("Hi");
+
+    //comment
     std::cout << "Hello World!" << std::endl;
+
+
     return 0;
 }
 ```
